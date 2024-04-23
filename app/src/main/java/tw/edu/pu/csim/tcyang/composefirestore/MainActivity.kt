3a2c6721cd3b392ca4c9939557fc0e7efbf18967
tcyang1971 <tcyang@gm.pu.edu.tw>
2024-04-23 09:10:11 +0800

    v1_TextField

--- app/src/main/java/tw/edu/pu/csim/tcyang/composefirestore/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/tcyang/composefirestore/MainActivity.kt
@@ -1,16 +1,26 @@
 package tw.edu.pu.csim.tcyang.composefirestore
 
+import android.R.attr.label
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import tw.edu.pu.csim.tcyang.composefirestore.ui.theme.ComposeFireStoreTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,7 +32,8 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    //Greeting("Android")
+                    Birth()
                 }
             }
         }
@@ -30,17 +41,48 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun Birth(){
+    var userName by remember { mutableStateOf("楊子青")}
+    var userWeight by remember { mutableStateOf(3800)}
+    var userPassword by remember { mutableStateOf("")}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ComposeFireStoreTheme {
-        Greeting("Android")
+    Column {
+        TextField(
+            value = userName,
+            onValueChange = { newText ->
+                userName = newText
+            },
+            label = { Text("姓名") },
+            placeholder = { Text("請輸入您的姓名") }
+        )
+
+        TextField(
+            value = userWeight.toString(),
+            onValueChange = { newText ->
+                if (newText == ""){
+                    userWeight = 0
+                }
+                else{userWeight = newText.toInt()}
+            },
+            label = { Text("體重") },
+            keyboardOptions = KeyboardOptions
+                (keyboardType = KeyboardType.Number)
+        )
+
+        TextField(
+            value = userPassword,
+            onValueChange = { newText ->
+                userPassword = newText
+            },
+            label = { Text("密碼") },
+            placeholder = { Text(text = "請輸入您的密碼") },
+            visualTransformation = PasswordVisualTransformation(),
+            keyboardOptions = KeyboardOptions
+                (keyboardType = KeyboardType.Password)
+        )
+
+        Text("您輸入的姓名是：$userName \n您的出生體重為：$userWeight 公克" +
+                "\n您輸入的密碼是：$userPassword")
+
     }
 }
