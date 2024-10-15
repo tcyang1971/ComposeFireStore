1da1b51b94e4b02d2f8c1195daba40801fc955fe
tcyang1971 <tcyang@gm.pu.edu.tw>
2024-10-15 11:53:01 +0800

    v4_密碼輸入

--- app/src/main/java/tw/edu/pu/csim/tcyang/composefirestore/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/tcyang/composefirestore/MainActivity.kt
@@ -52,6 +52,7 @@ class MainActivity : ComponentActivity() {
 fun Birth(m: Modifier){
     var userName by remember { mutableStateOf("楊子青")}
     var userWeight by remember { mutableStateOf(3800)}
+    var userPassword by remember { mutableStateOf("")}
 
     Column {
         TextField(
@@ -77,7 +78,20 @@ fun Birth(m: Modifier){
                 (keyboardType = KeyboardType.Number)
         )
 
-        Text("您輸入的姓名是：$userName\n出生體重為：$userWeight 公克")
+        TextField(
+            value = userPassword,
+            onValueChange = { newText ->
+                userPassword = newText
+            },
+            label = { Text("密碼") },
+            placeholder = { Text(text = "請輸入您的密碼") },
+            visualTransformation = PasswordVisualTransformation(),
+            keyboardOptions = KeyboardOptions
+                (keyboardType = KeyboardType.Password)
+        )
+
+        Text("您輸入的姓名是：$userName\n出生體重為：$userWeight 公克"
+                + "\n密碼：$userPassword")
     }
 }
 
