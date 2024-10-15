491ccbd4482faf3b7847afa6f8bfa49bad6477c5
tcyang1971 <tcyang@gm.pu.edu.tw>
2024-10-15 11:18:31 +0800

    v1_引用及ComposableFunction

--- app/src/main/java/tw/edu/pu/csim/tcyang/composefirestore/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/tcyang/composefirestore/MainActivity.kt
@@ -13,6 +13,20 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import tw.edu.pu.csim.tcyang.composefirestore.ui.theme.ComposeFireStoreTheme
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.material3.Button
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,10 +34,13 @@ class MainActivity : ComponentActivity() {
         setContent {
             ComposeFireStoreTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    /*
                     Greeting(
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
                     )
+                     */
+                    Birth(m = Modifier.padding(innerPadding))
                 }
             }
         }
@@ -31,17 +48,7 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Birth(m: Modifier){
+
 }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ComposeFireStoreTheme {
-        Greeting("Android")
-    }
-}
